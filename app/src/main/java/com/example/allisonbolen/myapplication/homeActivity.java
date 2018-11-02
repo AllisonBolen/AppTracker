2f3bb30e7570f65a17d44dfcb313f413f5ef3ac1
Brendan Nahed <brendannahed@Brendans-MacBook-Pro-2.local>
2018-11-02 11:06:02 -0400

    Completed Homework 8

--- app/src/main/java/com/example/allisonbolen/myapplication/homeActivity.java
+++ app/src/main/java/com/example/allisonbolen/myapplication/homeActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TableLayout;
@@ -18,7 +21,12 @@ public class homeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        TableLayout home_activity_layout = findViewById(R.id.home_activity_layout);
+        RecyclerView home_activity_layout = findViewById(R.id.home_activity_layout);
+        RecyclerView.Adapter mAdapter;
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+
+
+        home_activity_layout.setLayoutManager(mLayoutManager);
 
         FloatingActionButton fab =  findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,7 +34,7 @@ public class homeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                TableRow tr1 = new TableRow(getApplicationContext());
+                CardView tr1 = new CardView(getApplicationContext());
                 tr1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,TableRow.LayoutParams.MATCH_PARENT));
                 TextView textview = new TextView(getApplicationContext());
                 textview.setText("Hello World");
