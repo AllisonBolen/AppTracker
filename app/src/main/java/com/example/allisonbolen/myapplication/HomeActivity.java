7e1191b7c7e95403fdb644ffbdffbd0d76da94b6
Brendan Nahed <brendannahed@Brendans-MacBook-Pro-2.local>
2018-11-15 21:09:56 -0500

    New App object activity

--- app/src/main/java/com/example/allisonbolen/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/allisonbolen/myapplication/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.allisonbolen.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -28,13 +29,15 @@ public class HomeActivity extends AppCompatActivity
 
 
         FloatingActionButton fab =  findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-            }
+       fab.setOnClickListener(v->{
+           Intent newAppObject = new Intent(this, new_application_object.class);
+           startActivity(newAppObject);
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//
+//            }
         });
     }
 
