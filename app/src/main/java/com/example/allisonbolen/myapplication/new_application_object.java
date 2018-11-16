93d6bdc8c5657b522b49179a45215f2f4ef4ae04
Brendan Nahed <brendannahed@Brendans-MacBook-Pro-2.local>
2018-11-15 21:38:59 -0500

    Correct Strings

--- app/src/main/java/com/example/allisonbolen/myapplication/new_application_object.java
+++ app/src/main/java/com/example/allisonbolen/myapplication/new_application_object.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.allisonbolen.myapplication.dummy.DummyContent.*;
 import com.example.allisonbolen.myapplication.dummy.DummyContent;
@@ -24,9 +25,16 @@ public class new_application_object extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(v -> {
             DateTime now = new DateTime();
-            Application_Information_Object tempItem = new Application_Information_Object(findViewById(R.id.cmpyName).toString(), findViewById(R.id.cmpyDesc).toString(),
-                    findViewById(R.id.jbTitle).toString(), now, findViewById(R.id.jbDesc).toString(),
-                    now, findViewById(R.id.contactInfo).toString(), 1);
+            EditText cmpyName = findViewById(R.id.cmpyName);
+            EditText cmpyDesc = findViewById(R.id.cmpyDesc);
+            EditText jbTitle = findViewById(R.id.jbTitle);
+            EditText jbDesc = findViewById(R.id.jbDesc);
+            EditText contactInfo = findViewById(R.id.contactInfo);
+
+            Application_Information_Object tempItem = new Application_Information_Object(
+                    cmpyName.getText().toString(), cmpyDesc.getText().toString(),
+                    jbTitle.getText().toString(), now, jbDesc.getText().toString(),
+                    now, contactInfo.getText().toString(), 1);
             DummyContent.addItem(tempItem);
             finish();
 
