bc32c943a6b6c9974692fe4a6d673b9540346f09
Brendan Nahed <brendannahed@Brendans-MacBook-Pro-2.local>
2018-12-03 19:06:15 -0500

    LinkedIn SDK

--- app/src/main/java/com/example/allisonbolen/myapplication/EditProfileActivity.java
+++ app/src/main/java/com/example/allisonbolen/myapplication/EditProfileActivity.java
@@ -7,9 +7,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.allisonbolen.myapplication.dummy.DummyContent;
+import com.google.android.gms.common.api.Scope;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -32,6 +34,23 @@ public class EditProfileActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Button linkedIn_button = findViewById(R.id.connect_button);
+
+        linkedIn_button.setOnClickListener( v-> {
+
+        });
+    }
+
+    // Build the list of member permissions our LinkedIn session requires
+    private static Scope buildScope() {
+        return Scope.build(Scope.R_BASICPROFILE, Scope.W_SHARE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Add this line to your existing onActivityResult() method
+        LISessionManager.getInstance(getApplicationContext()).onActivityResult(this, requestCode, resultCode, data);
     }
 
 }
